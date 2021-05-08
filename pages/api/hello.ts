8b6ea2f84fcd4c3b1049f97dd79ee5c7dec338c2
Alice Davis <alice@gigantic.computer>
2021-05-08 00:03:43 -0700

    this should fix it, and it's an example of how to add types

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next provided Types https://nextjs.org/learn/excel/typescript/nextjs-types
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req, res) => {
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export default (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ name: 'John Doe' })
 }
