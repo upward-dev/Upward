627ad0ce86fab36953f8b9f78a9bae4cb8ecb902
lehman <luhlahlehman@gmail.com>
2021-05-19 13:11:15 -0700

    add real copy for About, add google form links to Home

--- pages/about.tsx
+++ pages/about.tsx
@@ -21,11 +21,19 @@ function About() {
         </div>
         <div className={styles.aboutDescription}>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam neque
-            odio, tincidunt nec neque in, lacinia tincidunt ligula. Morbi
-            laoreet eros quis ipsum dictum aliquet. Morbi pulvinar, diam dapibus
-            aliquam dapibus, felis orci tincidunt quam, sit amet efficitur mi
-            massa at lorem. Aliquam blandit sed nunc vel convallis.
+            This diversity project started to help give engineers{' '}
+            <b>hands-on experience</b> with new and popular technologies used at
+            startups, such as <b>React</b> and <b>Node.js</b>. Our goal is to
+            create a web application to help <b>advance women in tech</b>{' '}
+            through study groups and <b>mentorship</b>. Our web application is
+            an <b>open source project</b> that women engineers work on to build
+            features on a live application that both <b>builds their skills</b>,
+            and <b>benefits the community</b> as a whole.
+          </p>
+          <p>
+            Get involved by signing up for the waitlist to join one of our study
+            groups, or by signing up to get mentorship and career development
+            support as you find your next role with Upward.
           </p>
         </div>
         <div className={styles.people}>
@@ -35,13 +43,13 @@ function About() {
               image="/marianebekker.jpeg"
               role="Founder &amp; CEO"
               linkedin="https://www.linkedin.com/in/marianebekker/"
-              bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam neque odio, tincidunt nec neque in, lacinia tincidunt ligula. Morbi laoreet eros quis ipsum dictum aliquet. Morbi pulvinar, diam dapibus aliquam dapibus, felis orci tincidunt quam, sit amet efficitur mi massa at lorem. Aliquam blandit sed nunc vel convallis."></SpotlightCard>
+              bio="Mariane is a software engineer leader with over a decade of experience mentoring software engineers. She started her career as a software engineer after graduating from McGill University in Canada and then moved to San Francisco.  Her passion and super power is mentoring other women in tech to close the gender gap."></SpotlightCard>
             <SpotlightCard
               name="Alice Davis"
               role="Founder &amp; Developer"
               image="/alicedavis.jpg"
               linkedin="https://www.linkedin.com/in/diffalot/"
-              bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam neque odio, tincidunt nec neque in, lacinia tincidunt ligula. Morbi laoreet eros quis ipsum dictum aliquet. Morbi pulvinar, diam dapibus aliquam dapibus, felis orci tincidunt quam, sit amet efficitur mi massa at lorem. Aliquam blandit sed nunc vel convallis."></SpotlightCard>
+              bio="Alice aims to contribute to secured and anonymized, in-browser communications and data sharing platforms built on open web standards to amplify voices of marginalized communities. She prioritizes mentoring members of marginalized groups as they pursue commerce as web developers and entrepreneurs. She really likes making websites, native applications, and embedded devices that make people's lives easier and safer."></SpotlightCard>
           </div>
           <div className={styles.row}>
             <SpotlightCard
@@ -49,13 +57,13 @@ function About() {
               role="Founder &amp; Developer"
               image="/louiselehman.jpeg"
               linkedin="https://www.linkedin.com/in/louiselehman/"
-              bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam neque odio, tincidunt nec neque in, lacinia tincidunt ligula. Morbi laoreet eros quis ipsum dictum aliquet. Morbi pulvinar, diam dapibus aliquam dapibus, felis orci tincidunt quam, sit amet efficitur mi massa at lorem. Aliquam blandit sed nunc vel convallis."></SpotlightCard>
+              bio="Louise is a Full Stack Software Engineer in the health and wellness industry. She loves having the opportunity to work and interact with people across differing disciplines, including product, architecture, UX, copy, research, and AI/ML. She aspires to be a humble teacher, a trustworthy and helpful collaborator, a good listener, an analytical thinker, a good friend, and to be graceful amidst chaos."></SpotlightCard>
             <SpotlightCard
               name="Lexi Lewtan"
               role="Founder &amp; Product Vision"
               image="/lexilewtan.png"
               linkedin="https://www.linkedin.com/in/lexilewtan/"
-              bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam neque odio, tincidunt nec neque in, lacinia tincidunt ligula. Morbi laoreet eros quis ipsum dictum aliquet. Morbi pulvinar, diam dapibus aliquam dapibus, felis orci tincidunt quam, sit amet efficitur mi massa at lorem. Aliquam blandit sed nunc vel convallis."></SpotlightCard>
+              bio="Lexi is the Founder &amp; CEO at Centered, building the MasterClass of Mental Health. Her interests and experience are in cofounder or leadership Operations or Go-to-Market positions."></SpotlightCard>
           </div>
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -29,13 +29,28 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="#" className={styles.circle}>
+          <a
+            href="https://forms.gle/bXWayRQiW6V5UJxE6"
+            target="_blank"
+            className={styles.circle}>
             <h2>Sign Up &rarr;</h2>
             <p>Request to be part of a study group.</p>
           </a>
 
           <a
-            href="https://calendly.com/mariane-connect"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSdC-AbV2SNhMS99yhY6r4Gu34UC9DhVWIM7utII6irV1pyaSw/viewform"
+            target="_blank"
+            className={styles.circle}>
+            <h2>Find Your Next Role &rarr;</h2>
+            <p>
+              Request to get connected directly with hiring managers at fast
+              growing, inclusive and diverse startups.
+            </p>
+          </a>
+
+          <a
+            href="https://www.upwardtalent.co/"
+            target="_blank"
             className={styles.circle}>
             <h2>Upward &rarr;</h2>
             <p>
@@ -44,7 +59,10 @@ export default function Home() {
             </p>
           </a>
 
-          <a href="https://github.com/code-circles" className={styles.circle}>
+          <a
+            href="https://github.com/code-circles"
+            target="_blank"
+            className={styles.circle}>
             <h2>Open Source &rarr;</h2>
             <p>Check out the code that fuels this project.</p>
           </a>
