d3e0185bd9a52faf6c1cab9f776ce5227d8fe54d
Mariane <mariane@Mac.local>
2021-06-02 20:14:07 -0700

    added prisma

---
+++ index.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  console.log("hello")
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+}
+
+main()
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
+
