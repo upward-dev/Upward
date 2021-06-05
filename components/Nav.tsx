8dcb331ca1d2e763b56db968f8da5d5f722d70bd
ainneo <ainneo@gmail.com>
2021-06-04 23:33:20 -0700

    testing cdn links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,8 +2,55 @@
 
 const Nav = () => {
   return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
 
-<nav className="navbar  navbar-light bg-light">
+
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+{/* 
+    <button class="btn btn-secondary dropdown-toggl" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" > */}
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Separated link</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+  )
+}
+
+export default Nav
+
+{/* <nav className="navbar  navbar-light bg-light">
   <div className="container-fluid">
       <a className="navbar-brand" href="/">
           <img src="/upward-logo.png" width="150" alt="upward-logo"></img>
@@ -16,25 +63,20 @@ const Nav = () => {
     </button> */}
 
     {/* <div className="collapse navbar-collapse" id="navbarColor03"> */}
-      <ul className="nav justify-content-end">
-        <li className="nav-item">
-           <a style={{color: "black"}} className="nav-link" href="/about">About</a>
-        </li>
-        <li style={{backgroundColor:"#f3ced6",  borderRadius:10}}>
-        <a 
-          href="https://docs.google.com/forms/d/e/1FAIpQLSe5XunCtzemmzIZK9VTRMgoueC4_9Q8Ay1PycGxEa5uXgCjNg/viewform" 
-          className="btn btn- active"
-          style={{color: "black"}}
-          >
-          <i className="glyphicon-floppy-disk" aria-hidden="true"></i> Sign Up
-        </a> 
-    </li>
-      </ul>
-    </div>
-  {/* </div> */}
-</nav> 
-  )
-}
-
-export default Nav
-
+    //   <ul className="nav justify-content-end">
+    //     <li className="nav-item">
+    //        <a style={{color: "black"}} className="nav-link" href="/about">About</a>
+    //     </li>
+    //     <li style={{backgroundColor:"#f3ced6",  borderRadius:10}}>
+    //     <a 
+    //       href="https://docs.google.com/forms/d/e/1FAIpQLSe5XunCtzemmzIZK9VTRMgoueC4_9Q8Ay1PycGxEa5uXgCjNg/viewform" 
+    //       className="btn btn- active"
+    //       style={{color: "black"}}
+    //       >
+    //       <i className="glyphicon-floppy-disk" aria-hidden="true"></i> Sign Up
+    //     </a> 
+    // </li>
+    //   </ul>
+    // </div>
+  {/* </div> 
+</nav>  */}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import "../styles/globals.scss";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
@@ -12,8 +13,9 @@ function MyApp({ Component, pageProps }) {
         <title>Upward</title>
         <meta name="description" content="Advancing women in tech" />
         <link rel="icon" href="/upward-logo.png" />
-        {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" 
-          integrity="undefined" crossOrigin="anonymous"></script>        */}
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
       </Head>
       <Component {...pageProps} />
     </Layout>
