a2043f0981e7f3f88dbc65961fc3918500568d7a
ainneo <ainneo@gmail.com>
2021-06-06 05:35:22 -0700

    sass is fully setup

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,8 +5,10 @@ const Nav = () => {
         <a className="navbar-brand" href="/">
           <img src="/upward-logo.png" width="150" alt="upward-logo"></img>
         </a>
+        {/* 
+        **** button for when we add more documents do NOT delete!
         <button
-          className="navbar-toggler"
+          className="navbar-toggler primary"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarColor01"
@@ -14,8 +16,9 @@ const Nav = () => {
           aria-expanded="false"
           aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarColor01">
+        </button> */}
+        {/* <div className="collapse navbar-collapse" id="navbarColor01"> */}
+        <div className="primary" id="navbarColor01">
           <ul className="nav ms-auto">
             <li className="nav-item">
               <a style={{ color: 'black' }} className="nav-link" href="/about">
@@ -27,7 +30,7 @@ const Nav = () => {
                 href="https://docs.google.com/forms/d/e/1FAIpQLSe5XunCtzemmzIZK9VTRMgoueC4_9Q8Ay1PycGxEa5uXgCjNg/viewform"
                 className="btn btn- active"
                 style={{ color: 'black' }}>
-                <i className="glyphicon-floppy-disk" aria-hidden="true"></i>{' '}
+                <i className="glyphicon-floppy-disk" aria-hidden="true"></i>
                 Sign Up
               </a>
             </li>
@@ -39,40 +42,3 @@ const Nav = () => {
 }
 
 export default Nav
-
-{
-  /* <nav className="navbar  navbar-light bg-light">
-  <div className="container-fluid">
-      <a className="navbar-brand" href="/">
-          <img src="/upward-logo.png" width="150" alt="upward-logo"></img>
-       </a>
-    {/* <button className="navbar-toggler" type="button" 
-      data-bs-toggle="collapse" data-bs-target="#navbarColor03" 
-      aria-controls="navbarColor03" aria-expanded="false" 
-      aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button> */
-}
-
-{
-  /* <div className="collapse navbar-collapse" id="navbarColor03"> */
-}
-//   <ul className="nav justify-content-end">
-//     <li className="nav-item">
-//        <a style={{color: "black"}} className="nav-link" href="/about">About</a>
-//     </li>
-//     <li style={{backgroundColor:"#f3ced6",  borderRadius:10}}>
-//     <a
-//       href="https://docs.google.com/forms/d/e/1FAIpQLSe5XunCtzemmzIZK9VTRMgoueC4_9Q8Ay1PycGxEa5uXgCjNg/viewform"
-//       className="btn btn- active"
-//       style={{color: "black"}}
-//       >
-//       <i className="glyphicon-floppy-disk" aria-hidden="true"></i> Sign Up
-//     </a>
-// </li>
-//   </ul>
-// </div>
-{
-  /* </div> 
-</nav>  */
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-
-
-
 
 export default function Home() {
   return (
@@ -13,45 +8,39 @@ export default function Home() {
         <meta name="description" content="Advancing women in tech" />
       </Head>
 
-      <div className="text-center">
-       
-          <h1 className="title">Upward</h1>
+      <div className="text-center page-margin-top">
+        <button type="button" className="btn btn-primary">
+          Primary
+        </button>
+        <h1 className="title">Upward</h1>
         <p>
-          Our mission is to advance women in tech. We provide this platform to help you land an awesome role!
+          Our mission is to advance women in tech. We provide this platform to
+          help you land an awesome role!
         </p>
 
         <div className="container">
           <div className="row">
             <div className="col-sm">
-              
-                <h4>Sign Up</h4>
-                <a
-                href="https://forms.gle/bXWayRQiW6V5UJxE6"
-                target="_blank"
-                >
-
+              <h4>Sign Up</h4>
+              <a href="https://forms.gle/bXWayRQiW6V5UJxE6" target="_blank">
                 <p>Request to be part of a study group.</p>
               </a>
             </div>
 
             <div className="col-sm">
-           
               <h4>Find Your Next Role</h4>
-            <a
-              href="https://docs.google.com/forms/d/e/1FAIpQLSdC-AbV2SNhMS99yhY6r4Gu34UC9DhVWIM7utII6irV1pyaSw/viewform"
-              target="_blank"
-             >
-              <p>
-                Request to get connected directly with hiring managers at fast
-                growing, inclusive and diverse startups.
-              </p>
-            </a>
-          </div>
+              <a
+                href="https://docs.google.com/forms/d/e/1FAIpQLSdC-AbV2SNhMS99yhY6r4Gu34UC9DhVWIM7utII6irV1pyaSw/viewform"
+                target="_blank">
+                <p>
+                  Request to get connected directly with hiring managers at fast
+                  growing, inclusive and diverse startups.
+                </p>
+              </a>
+            </div>
           </div>
         </div>
       </div>
     </div>
   )
 }
-
-
