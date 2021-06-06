2507783e9673c634632c74612acc6a6c4a9ec8d8
Mariane <mariane@Mac.local>
2021-06-05 20:48:33 -0700

    added prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -18,7 +18,7 @@ let prisma: PrismaClient
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
 } else {
-  if (!global && !global.prisma) {
+  if (!global.prisma) {
     global.prisma = new PrismaClient()
   }
   prisma = global.prisma
