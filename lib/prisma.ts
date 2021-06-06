288e83239148ad588200cd78b758c2b148b3eb45
Mariane <mariane@Mac.local>
2021-06-05 20:46:22 -0700

    fixing global error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -11,7 +11,7 @@ let prisma: PrismaClient
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
 } else {
-  if (!global.prisma) {
+  if (!global && !global.prisma) {
     global.prisma = new PrismaClient()
   }
   prisma = global.prisma
