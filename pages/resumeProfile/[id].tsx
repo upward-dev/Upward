f43ff87f919438fce9418fa5c8199a3ed4df32de
rach-con13 <rachaelconcessio+@gmail.com>
2021-06-08 10:34:57 -0400

    resumeProfile: removed spacing

--- pages/resumeProfile/[id].tsx
+++ pages/resumeProfile/[id].tsx
@@ -1,8 +1,6 @@
 import { useRouter } from 'next/dist/client/router';
 import { route } from 'next/dist/next-server/server/router'
 import React, { useEffect, useState } from 'react'
-
-
 // needs to be replaced with real api call for data for final
 enum Role {
     USER = 'User',
@@ -13,7 +11,6 @@ enum Role {
 interface Profile {
     calendlyLink: string
 }
-
 interface iUser {
     id: number;
     firstName: string;
@@ -26,8 +23,6 @@ interface iUser {
     profile: Profile
 
 }
-
-
 // fake user data
 const users: iUser[] = [
     {
@@ -56,18 +51,11 @@ const users: iUser[] = [
             calendlyLink: "davidlink"
         }
     }
-
 ];
-
-
-
-
-
 export default function ResumeProfile() {
     const router = useRouter();
     const [currentUser, setUser] = useState<iUser | null>(null);
     const { id } = router.query;
-
     useEffect(() => {
         let findCurrentUser = users.filter(user => user.id === Number(id));
         if (findCurrentUser.length > 0) {
@@ -75,16 +63,11 @@ export default function ResumeProfile() {
         }
 
     }, [id, setUser])
-
     const { firstName, lastName, email, createdAt, updatedAt, role, tasks, profile } = currentUser || {};
-
-
-
     return (
         <div style={{ marginTop: '200px' }} className="container">
             <div style={{ display: "flex", alignItems: "baseline" }}>
                 <h4>{firstName} {lastName}</h4>
-
             </div>
             <div>
                 <ul style={{ listStyleType: 'none' }}>
@@ -94,25 +77,19 @@ export default function ResumeProfile() {
                     <li>
                         <p>Calendly Link: <span>{profile && profile.calendlyLink}</span></p>
                     </li>
-
                     <li>
                         <p>role <span>{role}</span></p>
                     </li>
-
                 </ul>
                 <ul>
                     <h4>Tasks</h4>
                     {tasks && tasks.map((task, index) => {
                         return (
-
-
                             <li key={index}>{task}</li>
-
                         )
                     })}
                 </ul>
             </div>
-
         </div>
     )
 }
