a69095a66f21a1a1963a4c80641b7c9ea2fa94cc
Mariane <mariane@Mac.local>
2021-06-12 08:21:05 -0700

    minor change

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,11 +1,7 @@
-import { faMagic } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
-import {Magic} from "magic-sdk";
 import { useForm } from 'react-hook-form';
 
-function LoginForm({submitForm}) {
-
-
+function LoginForm() {
     const onSubmit = async (e: object) => {
         // e.preventDefault();
         // await primsa db response
@@ -17,15 +13,15 @@ function LoginForm({submitForm}) {
     };
 
     const {register, handleSubmit} = useForm({
-     
         // onSubmit,
         // validate
     });
+
     const userEmail = register('userEmail');
     const userPassword = register('userPassword');
     
     return(
-        <form onSubmit={handleSubmit(submitForm)} className="border rounded p-3">
+        <form  className="border rounded p-3">
             <h1 className="text-center fs-3">Welcome Back!</h1>
             <h2 className="text-center fs-5 mb-4">Login to your Upward account.</h2>
             <div className="mb-3">
@@ -41,7 +37,7 @@ function LoginForm({submitForm}) {
                 )}*/}
             </div>
             <div className="d-grid gap-2 col-6 mx-auto">
-                <button type="submit"className="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary">Submit</button>
             </div>
             <div className="text-center my-2">
                 New to Upward? <a href="/register">Register now</a>
