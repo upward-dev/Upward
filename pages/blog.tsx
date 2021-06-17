66602c23ac9e52da4b061647fb68084ef1257cda
Mariane <mariane@Mac.local>
2021-06-16 20:37:11 -0700

    fixed typo

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -4,10 +4,6 @@ function Blog() {
   return (       
     <body>
         <iframe src="https://upwardrecruiting.com/blog"
-                frameborder="0" 
-                marginheight="0" 
-                margintop = "-100px"
-                marginwidth="0" 
                 width="100%" 
                 height="1000px" 
                 scrolling="auto"
